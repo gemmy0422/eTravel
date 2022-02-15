823310bb3695a199c2466bd5b13b1f7ad5860cab
Gemmy <gemmy0422@gmail.com>
2022-02-15 21:39:51 +0800

    Updates

---
+++ precache-manifest.e233a4bba92611d9f517a11262238c2d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ebf39da42af6e5b0b85",
+    "url": "/css/app.c5711825.css"
+  },
+  {
+    "revision": "2be11bea88c12fae6dff8fbc9b6f0aab",
+    "url": "/img/airplane.2be11bea.svg"
+  },
+  {
+    "revision": "08519b8922349e38a6ff924b8ca362cf",
+    "url": "/img/bed.08519b89.svg"
+  },
+  {
+    "revision": "95afc0d5e9bfb3c90b0cadcf7e3b0ed3",
+    "url": "/img/car.95afc0d5.svg"
+  },
+  {
+    "revision": "974397c07bd559c43c97e3982babbcea",
+    "url": "/img/down-arrow.974397c0.svg"
+  },
+  {
+    "revision": "c162beff2f8295757a4c1dbd59c0ab65",
+    "url": "/img/down-chevron.c162beff.svg"
+  },
+  {
+    "revision": "bd9972cedb25bae442a7b4d3ef030fea",
+    "url": "/img/facebook.bd9972ce.svg"
+  },
+  {
+    "revision": "5693085da8f36783356512ffadb10237",
+    "url": "/img/line.5693085d.svg"
+  },
+  {
+    "revision": "6224a421cb58d0a470cb47c3c2a8d759",
+    "url": "/img/logo.6224a421.jpg"
+  },
+  {
+    "revision": "1a47c5981af9474dde7cd8a99cc17b3e",
+    "url": "/img/telegram.1a47c598.svg"
+  },
+  {
+    "revision": "f4aa0592e76709bfb72fe8849b73f65c",
+    "url": "/img/youtube.f4aa0592.svg"
+  },
+  {
+    "revision": "0edb82d134aee1b7d6037071f4fdb54f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ebf39da42af6e5b0b85",
+    "url": "/js/app.06a9b6bc.js"
+  },
+  {
+    "revision": "a268c45198a39ee69183",
+    "url": "/js/chunk-vendors.c6852701.js"
+  },
+  {
+    "revision": "374f32a80f8c54b9dfaf331595da1d65",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
