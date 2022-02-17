3f62f204bab3b3a85731c7e5e2b37d9fe3b610a6
Gemmy <gemmy0422@gmail.com>
2022-02-18 00:10:19 +0800

    modify: 調整manifest

---
+++ build/precache-manifest.ce6b1f722c0e1ea8bd7b1381dfb15b24.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc5cccb2211e0f5db9e",
+    "url": "/eTravel/css/app.df296805.css"
+  },
+  {
+    "revision": "2be11bea88c12fae6dff8fbc9b6f0aab",
+    "url": "/eTravel/img/airplane.2be11bea.svg"
+  },
+  {
+    "revision": "08519b8922349e38a6ff924b8ca362cf",
+    "url": "/eTravel/img/bed.08519b89.svg"
+  },
+  {
+    "revision": "95afc0d5e9bfb3c90b0cadcf7e3b0ed3",
+    "url": "/eTravel/img/car.95afc0d5.svg"
+  },
+  {
+    "revision": "974397c07bd559c43c97e3982babbcea",
+    "url": "/eTravel/img/down-arrow.974397c0.svg"
+  },
+  {
+    "revision": "c162beff2f8295757a4c1dbd59c0ab65",
+    "url": "/eTravel/img/down-chevron.c162beff.svg"
+  },
+  {
+    "revision": "bd9972cedb25bae442a7b4d3ef030fea",
+    "url": "/eTravel/img/facebook.bd9972ce.svg"
+  },
+  {
+    "revision": "5693085da8f36783356512ffadb10237",
+    "url": "/eTravel/img/line.5693085d.svg"
+  },
+  {
+    "revision": "6224a421cb58d0a470cb47c3c2a8d759",
+    "url": "/eTravel/img/logo.6224a421.jpg"
+  },
+  {
+    "revision": "1a47c5981af9474dde7cd8a99cc17b3e",
+    "url": "/eTravel/img/telegram.1a47c598.svg"
+  },
+  {
+    "revision": "f4aa0592e76709bfb72fe8849b73f65c",
+    "url": "/eTravel/img/youtube.f4aa0592.svg"
+  },
+  {
+    "revision": "51b95c072ac602e819bfdc214977d218",
+    "url": "/eTravel/index.html"
+  },
+  {
+    "revision": "acc5cccb2211e0f5db9e",
+    "url": "/eTravel/js/app.8218955b.js"
+  },
+  {
+    "revision": "c5ebf2ccaab7fbf36413",
+    "url": "/eTravel/js/chunk-vendors.55b98432.js"
+  },
+  {
+    "revision": "374f32a80f8c54b9dfaf331595da1d65",
+    "url": "/eTravel/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/eTravel/robots.txt"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/eTravel/precache-manifest.a86154a0781acf45bf535eb30fe846dd.js"
+  "/eTravel/precache-manifest.64a406a3951599825b3941ed2ea9e998.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "etravel"});
